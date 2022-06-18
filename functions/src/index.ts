f9457bf3a7c2cae276990034feb7c898ac0e6ff6
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-06-18 18:13:44 -0400

    added pupetter and openai gnereation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,41 @@
 import * as functions from "firebase-functions";
 import { Configuration, OpenAIApi } from "openai";
+const puppeteer = require("puppeteer");
+const Alpaca = require("@alpaca/alpaca-trade-api");
 
 const configuration = new Configuration({
-  apiKey: functions.config().openai.key,
+  apiKey: process.env.OPENAI_API_KEY,
 });
-
 const openai = new OpenAIApi(configuration);
+
+const alpaca = new Alpaca({
+  paper:true
+})
+
+
+async function scrape() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.capitoltrades.com/politicians/P000197", {
+    waitUntil: "networkidle2",
+  });
+  await page.waitForTimeout(3000);
+
+  await page.evaluate(() => {
+    return document.body.innerText;
+  });
+  await browser.close();
+}
 
 export const helloWorld = functions.https.onRequest(
   async (request, response) => {
+
+    const data = await scrape();
+
     const gptCompletetion = await openai.createCompletion({
       model: "text-davinci-002",
-      prompt: "Nancy Pelosi would recommend buying the following stocks:",
+      prompt:
+        `${data} Nancy Pelosi would recommend buying the following stock tickers:`,
       temperature: 0.7,
       max_tokens: 32,
       top_p: 1,
