73deda1fe51874761a4a750c0fc35448b3882b98
Nima Pourjafar <nima.pourjafar123@gmail.com>
2022-06-18 21:16:00 -0400

    added stock trading

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from "firebase-functions";
-import { Configuration, OpenAIApi } from "openai";
+import {Configuration, OpenAIApi} from "openai";
 const puppeteer = require("puppeteer");
 const Alpaca = require("@alpaca/alpaca-trade-api");
 
@@ -9,9 +9,10 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const alpaca = new Alpaca({
-  paper:true
-})
-
+  paper: true,
+  keyId: process.env.ALPACA_API_ID,
+  secretKey: process.env.ALPACA_API_KEY,
+});
 
 async function scrape() {
   const browser = await puppeteer.launch();
@@ -27,22 +28,43 @@ async function scrape() {
   await browser.close();
 }
 
-export const helloWorld = functions.https.onRequest(
-  async (request, response) => {
+exports.trader = functions
+    .runWith({memory: "4GB"})
+    .pubsub.schedule("0 10 * * 1-5")
+    .timeZone("America/New_York")
+    .onRun(async (ctx) => {
+      const data = await scrape();
 
-    const data = await scrape();
+      const gptCompletetion = await openai.createCompletion({
+        model: "text-davinci-002",
+        prompt: `${data} Nancy Pelosi would recommend buying the following stock tickers:`,
+        temperature: 0.7,
+        max_tokens: 32,
+        top_p: 1,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0,
+      });
 
-    const gptCompletetion = await openai.createCompletion({
-      model: "text-davinci-002",
-      prompt:
-        `${data} Nancy Pelosi would recommend buying the following stock tickers:`,
-      temperature: 0.7,
-      max_tokens: 32,
-      top_p: 1,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
+      const stocksToBuy =
+      // @ts-ignore
+      gptCompletetion.data.choices[0].text?.match(/\[A-Z]+\b/g);
+      console.log("STOCKS TO BUY:", stocksToBuy);
+
+      if (stocksToBuy === null) {
+        console.log("sitting this one out");
+        return null;
+      }
+
+      const order = await alpaca.createOrder({
+      // @ts-ignore
+        symbol: stocksToBuy[0],
+        notional: 1000,
+        side: "buy",
+        type: "market",
+        time_in_force: "day",
+      });
+
+      console.log("ORDER ID:", order.id);
+
+      return null;
     });
-
-    response.send(gptCompletetion.data);
-  }
-);
